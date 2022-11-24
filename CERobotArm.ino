c547aa472bfd80587c7bf49828749716336b41d7
unknown <jackcrever@gmail.com>
2022-11-24 23:16:17 +0100

    wip

--- CERobotArm.ino
+++ CERobotArm.ino
@@ -1,3 +1,4 @@
+
 #include <Stepper.h>
 #include <assert.h>
 
@@ -14,6 +15,12 @@ const int J2stepPin = 6;
 const int J2dirPin = 7;
 const int J3stepPin = 10;
 const int J3dirPin = 11;
+const int J4stepPin = 23;
+const int J4dirPin = 25;
+const int J5stepPin = 12;
+const int J5dirPin = 13;
+const int J6stepPin = 9;
+const int J6dirPin = 4;
 
 #define PI 3.1415926535897932384626433832795
 
@@ -27,29 +34,45 @@ int delayvar = 10;
 int16_t thd0[50];
 int16_t thd1[50];
 int16_t thd2[50];
+int16_t thd3[50];
+int16_t thd4[50];
+int16_t thd5[50];
 
+Stepper baseStepper(stepsPerRevolution, J1stepPin, J1dirPin); // step 8 dir 9
+Stepper rot1Stepper(steps1PerRevolution, J2stepPin, J2dirPin);
+Stepper rot2Stepper(stepsPerRevolution, J3stepPin, J3dirPin);
+Stepper J4Stepper(stepsPerRevolution, J4stepPin, J4dirPin);
+Stepper J5Stepper(stepsPerRevolution, J5stepPin, J5dirPin);
+Stepper J6Stepper(stepsPerRevolution, J6stepPin, J6dirPin);
 
-Stepper baseStepper(stepsPerRevolution, 8, 5); // step 8 dir 9
-Stepper rot1Stepper(steps1PerRevolution, 6, 7);
-Stepper rot2Stepper(stepsPerRevolution, 10, 11);
-
-
-void calculator(int32_t J1step, int32_t J2step, int32_t J3step)
+void calculator(int32_t J1step, int32_t J2step, int32_t J3step, int32_t J4step, int32_t J5step, int32_t J6step)
 {
   int curDelay = 1;
   int J1cur = 0;
   int J2cur = 0;
   int J3cur = 0;
+  int J4cur = 0;
+  int J5cur = 0;
+  int J6cur = 0;
   int J1el;
   int J2el;
   int J3el;
+  int J4el;
+  int J5el;
+  int J6el;
   int a;
   int b;
   int c;
+  int d;
+  int e;
+  int f;
 
   a=abs(J1step);
   b=abs(J2step);
   c=abs(J3step);
+  a=abs(J4step);
+  b=abs(J5step);
+  c=abs(J6step);
 
 
   if (J1step < 0)
@@ -82,22 +105,65 @@ void calculator(int32_t J1step, int32_t J2step, int32_t J3step)
     J3el=1;
   }
 
+
+  if (J4step < 0)
+  {
+    J4el=-1;
+
+  }
+  else if (J4step > 0)
+  {
+    J4el=1;
+  }
+
+
+  if (J5step < 0)
+  {
+    J5el=-1;
+  }
+  else if (J5step > 0)
+  {
+    J5el=1;
+  }
+
+
+  if (J6step < 0)
+  {
+    J6el=-1;
+  }
+  else if (J6step > 0)
+  {
+    J6el=1;
+  }
+
   const int32_t max_step_size = 1;
   int32_t biggest = a;
   if (b > biggest)
     biggest = b;
   if (c > biggest)
     biggest = c;
+  if (d > biggest)
+    biggest = d;
+  if (e > biggest)
+    biggest = e;
+  if (f > biggest)
+    biggest = f;
 
   int32_t number_of_steps = ceil(biggest/max_step_size);
 
   double avg_a = (double)a / (double)number_of_steps;
   double avg_b = (double)b / (double)number_of_steps;
   double avg_c = (double)c / (double)number_of_steps;
+  double avg_d = (double)d / (double)number_of_steps;
+  double avg_e = (double)e / (double)number_of_steps;
+  double avg_f = (double)f / (double)number_of_steps;
 
   int32_t a_done = 0;
   int32_t b_done = 0;
   int32_t c_done = 0;
+  int32_t d_done = 0;
+  int32_t e_done = 0;
+  int32_t f_done = 0;
 
   for (int i = 0; i < number_of_steps; ++i) {
     int32_t curr_a = floor(avg_a*i);
@@ -106,14 +172,26 @@ void calculator(int32_t J1step, int32_t J2step, int32_t J3step)
     curr_b -= b_done;
     int32_t curr_c = floor(avg_c*i);
     curr_c -= c_done;
+    int32_t curr_d = floor(avg_d*i);
+    curr_d -= d_done;
+    int32_t curr_e = floor(avg_e*i);
+    curr_e -= e_done;
+    int32_t curr_f = floor(avg_f*i);
+    curr_f -= f_done;
 
     a -= curr_a;
     b -= curr_b;
     c -= curr_c;
+    d -= curr_d;
+    e -= curr_e;
+    f -= curr_f;
 
     a_done += curr_a;
     b_done += curr_b;
     c_done += curr_c;
+    d_done += curr_d;
+    e_done += curr_e;
+    f_done += curr_f;
 
     baseStepper.step(curr_a*J1el);
     delayMicroseconds(curDelay);
@@ -121,13 +199,19 @@ void calculator(int32_t J1step, int32_t J2step, int32_t J3step)
     delayMicroseconds(curDelay);
     rot2Stepper.step(curr_c*J3el);
     delayMicroseconds(curDelay);
+    J4Stepper.step(curr_d*J4el);
+    delayMicroseconds(curDelay);
+    J5Stepper.step(curr_f*J5el);
+    delayMicroseconds(curDelay);
+    J6Stepper.step(curr_e*J6el);
+    delayMicroseconds(curDelay);
 
   }
 };
 
 void odavisz(){
   for (int i=0; i<sizeof thd0/sizeof thd0[0]; i++) {
-    calculator(thd0[i], thd1[i], thd2[i]);
+    calculator(thd0[i], thd1[i], thd2[i], thd3[i], thd4[i], thd5[i]);
     //Serial.println(thd0[i]);
     //Serial.println(thd1[i]);
     //Serial.println(thd2[i]);
@@ -152,6 +236,7 @@ void manual(){
     thet0=thet0+inInt/basediv;
     Serial.println(thet0);
   }
+
   // 12800 lépés 90 fok 100 speed 1 fok 142 lépés
   if (inChar=='e') {
     baseStepper.step(-inInt);
@@ -205,11 +290,13 @@ void setup() {
   baseStepper.setSpeed(100);
   rot1Stepper.setSpeed(250);
   rot2Stepper.setSpeed(50);
-
+  J4Stepper.setSpeed(50);
+  J5Stepper.setSpeed(50);
+  J6Stepper.setSpeed(50);
 }
 
 
-uint8_t bytes[320];
+uint8_t bytes[520];
 int indexBuff=0;
 
 
@@ -222,16 +309,16 @@ void loop() {
 
 
     Serial.setTimeout(10);
-    int len = Serial.readBytes(bytes,320);
+    int len = Serial.readBytes(bytes,520);
 
-    //uint8_t bytes[] = {56,130,87,0,85,0,83,0,81,0,79,0,78,0,76,0,74,0,73,0,71,0,70,0,68,0,67,0,65,0,64,0,63,0,61,0,60,0,59,0};
+    //int len[] = {56,130,87,0,85,0,83,0,81,0,79,0,78,0,76,0,74,0,73,0,71,0,70,0,68,0,67,0,65,0,64,0,63,0,61,0,60,0,59,0};
 
     switch (indexBuff) {
       case 0:
-        //Serial.println("th0");
+        Serial.println("th0");
         for (int i=0; i<len;i=i+2){
           int16_t result_0 = read_int16_t(bytes, i);
-          //Serial.println(result_0);
+          Serial.println(result_0);
           thd0[i/2]=result_0;
         }
         break;
@@ -239,28 +326,57 @@ void loop() {
         //Serial.println("th0");
         for (int i=0; i<len;i=i+2){
           int16_t result_0 = read_int16_t(bytes, i);
-          //Serial.println(result_0);
+          Serial.println(result_0);
           thd0[i/2]=result_0;
         }
         break;
 
       case 2:
-        //Serial.println("th1");
+        Serial.println("th1");
         for (int i=0; i<len;i=i+2){
           int16_t result_0 = read_int16_t(bytes, i);
-          //Serial.println(result_0);
+          Serial.println(result_0);
           thd1[i/2]=result_0;
         }
         break;
 
       case 3:
-        //Serial.println("th2");
+        Serial.println("th2");
         for (int i=0; i<len;i=i+2){
           int16_t result_0 = read_int16_t(bytes, i);
-          //Serial.println(result_0);
+          Serial.println(result_0);
           thd2[i/2]=result_0;
         }
         break;
+
+      case 4:
+        Serial.println("th3");
+        for (int i=0; i<len;i=i+2){
+          int16_t result_0 = read_int16_t(bytes, i);
+          Serial.println(result_0);
+          thd3[i/2]=result_0;
+        }
+        break;
+
+      case 5:
+        Serial.println("th4");
+        for (int i=0; i<len;i=i+2){
+          int16_t result_0 = read_int16_t(bytes, i);
+          Serial.println(result_0);
+          thd4[i/2]=result_0;
+        }
+        break;
+
+      case 6:
+        Serial.println("th5");
+        for (int i=0; i<len;i=i+2){
+          int16_t result_0 = read_int16_t(bytes, i);
+          Serial.println(result_0);
+          thd5[i/2]=result_0;
+        }
+        break;
+
+
       default:
 
           indexBuff=0;
@@ -268,7 +384,7 @@ void loop() {
 
     }
 
-    if (indexBuff==3){
+    if (indexBuff==6){
       odavisz();
     }
     indexBuff++;
--- inversKinematics.m
+++ inversKinematics.m
@@ -1,15 +1,21 @@
 
 
-syms  thet0 thet1 thet2  r1 r2 r0 alp0 d0
+syms  thet0 thet1 thet2 thet3 thet4 thet5 r1 r2  alp0 alp2 alp3 alp4  d0 d3 d5
 
 %alpn rn dn thetn
 alp0=-pi/2
+%thet0=0
+%thet1=0
+%thet2=0
+%thet3=0
+%thet4=0
+%thet5=0
 
-d0=0.19
-rn=r0
-thetn=thet0
-alpn=alp0
-dn=d0
+
+rn=0
+thetn=0
+alpn=0
+dn=d3
 
 Base = [cos(thetn) -sin(thetn)*cos(alpn) sin(thetn)*sin(alpn) rn*cos(thetn);
      sin(thetn) cos(thetn)*cos(alpn) -cos(thetn)*sin(alpn) rn*sin(thetn);
@@ -18,28 +24,55 @@ Base = [cos(thetn) -sin(thetn)*cos(alpn) sin(thetn)*sin(alpn) rn*cos(thetn);
 
 
 
-rn=r0
-thetn=thet0
 alpn=alp0
 dn=d0
 
-A0 = [cos(thet0) 0 -sin(thet0) r0*cos(thet0);
-     sin(thet0) 0 cos(thet0) r0*sin(thet0);
-     0 -1 0 dn;
+J1 = [cos(thet0) 0 -sin(thet0) 0;
+     sin(thet0) 0 cos(thet0) 0;
+     0 -1 0 d0;
      0 0 0 1]
 
-A =  [cos(thet1-pi/2) -sin(thet1-pi/2) 0 r1*cos(thet1-pi/2);
+J2 =  [cos(thet1-pi/2) -sin(thet1-pi/2) 0 r1*cos(thet1-pi/2);
      sin(thet1-pi/2) cos(thet1-pi/2) 0 r1*sin(thet1-pi/2);
      0 0 1 0;
      0 0 0 1]
-B =  [cos(thet2+pi/2) -sin(thet2+pi/2) 0 r2*cos(thet2+pi/2);
-     sin(thet2+pi/2) cos(thet2+pi/2) 0 r2*sin(thet2+pi/2);
-     0 0 1 0;
+J3 =  [cos(thet2) 0 -sin(thet2) r2*cos(thet2);
+     sin(thet2) 0 cos(thet2) r2*sin(thet2);
+     0 -1 0 0;
      0 0 0 1]
 
-C = A0*A*B
+J34 = [1 0 0 0;
+       0 1 0 0;
+       0 0 1 d3;
+       0 0 0 1]
 
-out = C(:,4:4) % Extract columns 3 and 4 only into a new 2D array.
+J4 = [cos(thet3) 0 -sin(thet3) 0;
+     sin(thet3) 0 cos(thet3) 0;
+     0 -1 0 0;
+     0 0 0 1]
+J5 = [cos(thet4) 0 sin(thet4) 0;
+     sin(thet4) 0 -cos(thet4) 0;
+     0 1 0 0;
+     0 0 0 1]
+J6 = [cos(thet5) -sin(thet5) 0 0;
+     sin(thet5) cos(thet5) 0 0;
+     0 0 1 d5;
+     0 0 0 1]
+
+
+Ei33= [1 0 0;
+       0 1 0;
+       0 0 1]
+
+T03 = simplify(J1*J2*J3*J34)
+T36 = simplify(J4*J5*J6)
+T06 = simplify(T03*T36)
+R06 = T06(1:3,1:3)
+R03 = simplify(T03(1:3,1:3))
+
+
+
+out = T03(:,4:4) % Extract columns 3 and 4 only into a new 2D array.
 x=out(1,:)
 y=out(2,:)
 z=out(3,:)
@@ -47,10 +80,10 @@ z=out(3,:)
 x=simplify(x)
 y=simplify(y)
 z=simplify(z)
-%or directly 
+%or directly
 %%S = simplify(sin(x)^2 + cos(x)^2)
 
-j=jacobian([x,y,z],[thet0, thet1, thet2])
+j=jacobian([x,y,z,roll,pitch,yaw],[thet0, thet1, thet2, thet3, thet4, thet5])
 
 jinv=adjoint(j)/det(j)
 
